b8cd455acf2874e399b824ca35fe80974c6b9a9f
Harbatau <npikolist@gmail.com>
2020-08-03 16:52:27 +0300

    Updates

---
+++ precache-manifest.64be5a32cf523980311be38427a77913.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f2e9efb3ad7d77d4516ce67fb2babf8",
+    "url": "/portfolioV3/index.html"
+  },
+  {
+    "revision": "3aa7c44fb7390f0fb639",
+    "url": "/portfolioV3/static/css/main.7b05b5dc.chunk.css"
+  },
+  {
+    "revision": "72027eaf1152875b780b",
+    "url": "/portfolioV3/static/js/2.5d503799.chunk.js"
+  },
+  {
+    "revision": "1301fa84c30c6c0c2d538a85d1affc86",
+    "url": "/portfolioV3/static/js/2.5d503799.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3aa7c44fb7390f0fb639",
+    "url": "/portfolioV3/static/js/main.6d021d10.chunk.js"
+  },
+  {
+    "revision": "660f856d735f8ad77d65",
+    "url": "/portfolioV3/static/js/runtime-main.855ffbb5.js"
+  },
+  {
+    "revision": "257bdb11a1e1e71fdb06f6a2c80af12e",
+    "url": "/portfolioV3/static/media/Anders.257bdb11.ttf"
+  },
+  {
+    "revision": "c30c70e32046550a163b1b9ac09d52b7",
+    "url": "/portfolioV3/static/media/MADE-EvolveSansLight.c30c70e3.otf"
+  },
+  {
+    "revision": "08fed8a95c00ff39ffbeb98d940d420e",
+    "url": "/portfolioV3/static/media/MADE-EvolveSansLightEVO.08fed8a9.otf"
+  },
+  {
+    "revision": "eda852ee86060fad5a1966cf3e0548d6",
+    "url": "/portfolioV3/static/media/garamond.eda852ee.ttf"
+  }
+]);
